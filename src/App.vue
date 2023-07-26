94ac4bdff9473384346de3eeb01d3b79aac65dbb
xiaokai <465760751@qq.com>
2023-07-26 21:12:45 +0800

    通用后台管理

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <!-- <i class="el-icon-edit"></i>
+    <i class="el-icon-share"></i>
+    <i class="el-icon-delete"></i>
+    <el-button type="primary" icon="el-icon-search">搜索</el-button> -->
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+<style lang="less">
+html,body,h3,p {
+  margin: 0;
+  padding: 0;
+}
+</style>
